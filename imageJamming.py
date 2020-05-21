7e171f7d26a1b7c743de201fd749910cd734af0b
SEFIANE-EL OUADGHIRI M'hamed <56483502+mhamedouadghiri@users.noreply.github.com>
2020-05-21 13:43:04 +0000

    comments

--- imageJamming.py
+++ imageJamming.py
@@ -1,6 +1,5 @@
-# encrypt image but can decrypt because of modulo
-# encrypted rgb values are the real ones
-# takes too much time
+# encrypts image but cannot decrypt because of modulo
+# takes too much time (3min for 512*512px, 30sec for 256*256px)
 
 import time
 
@@ -27,24 +26,29 @@ width, height = img.width, img.height
 rgb = list()
 enc_rgb = list()
 
+# extract RGB values and store them in rgb list
 for i in range(width):
     for j in range(height):
         rgb.append(pix[i, j])
 
+# encryption
 x = x0
 for triplet in rgb:
     pixel = list()
-    for val in triplet:  # val is R then G the B
+    for val in triplet:  # val is R then G the B for each pixel
         x_min = interval_min + r * val
         x_max = interval_min + r * (val + 1)
         N = 0
         while x < x_min or x > x_max:
             x = f(x)
             N += 1
+        # problem of losing data here, when storing modulo of encrypted RGB values
         pixel.append(N % S)
     enc_rgb.append(tuple(pixel))
 
+# save encrypted image
 encImage = Image.new(img.mode, (width, height))
 encImage.putdata(enc_rgb)
 encImage.save("files/enc_" + fileName)
+
 print(time.time() - start)
